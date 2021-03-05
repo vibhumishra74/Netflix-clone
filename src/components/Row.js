c13cc08a465f5537c0351487f8cb1c630a6c82da
vibhumishra74 <mail.ashishmishra74@gmail.com>
2021-03-05 15:12:35 +0530

    video bug fix

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Youtube from "react-youtube";
-import movietrailer from "movie-trailer";
+import YouTube from "react-youtube";
+// import movieTrailer from "movie-trailer";
+import movieTrailer from "movie-trailer";
 import axios from "./axios";
 import "./row.css";
+import Apikey from "./apikey";
 
 function Row({ title, fetchUrl, islarge = false }) {
   const [Movie, setMovie] = useState([]);
   const [trailerurl, settrailerurl] = useState("");
+  // const [trailerurl, settrailerurl] = useState('https://www.youtube.com/watch?v=KzhcF6wAQ1U&t=3s&ab_channel=moviesclips');
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -15,26 +18,55 @@ function Row({ title, fetchUrl, islarge = false }) {
     }
     fetchData();
   }, [fetchUrl]);
-  console.log("movies from row", Movie);
+  // console.log("movies from row", Movie);
+  // options for react youtube
   const opts = {
     height: "400",
     width: "100%",
     playerVars: {
+      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
   const base_url = "https://image.tmdb.org/t/p/original/";
-  const handleclick = (movie) => {
+  const handleclick = async (movie) => {
     if (trailerurl) {
       settrailerurl("");
     } else {
       // movieTrailer npm module
-      movietrailer(movie?.name || "")
-        .then((url) => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          settrailerurl(urlParams.get("v"));
-        })
-        .catch((error) => console.log("youtube error", error));
+      //  await movieTrailer(movie?.name || "")
+      //     .then( (url)  => {
+      //       // url give all url we only take after v  https://www.youtube.com/watch?v=KzhcF6wAQ1U&t=3s&ab_channel=moviesclips
+      //       const  urlParams = new URLSearchParams( new URL(url).search);
+      //       settrailerurl( urlParams.get("v"));
+      //       console.log(
+      //         "trailerurl",
+      //         trailerurl,
+      //         "settrailerURL",
+      //         settrailerurl(urlParams.get("v"))
+      //       );
+      //     })
+      //     .catch((error) =>
+      //       console.log(
+      //         "youtube error",
+      //         error,
+      //         console.log(
+      //           "trailerurl",
+      //           trailerurl,
+      //           "settrailerURL",
+      //           settrailerurl("hello")
+      //         )
+      //       )
+      //     );
+
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=${Apikey}`
+      );
+      // console.log("trailerurl", trailerurl);
+      settrailerurl(trailerurl.data.results[0]?.key);
+      if (!trailerurl.data.results[0]?.key) {
+        alert("not abailable right now plese try another movies");
+      }
     }
   };
   return (
@@ -57,7 +89,7 @@ function Row({ title, fetchUrl, islarge = false }) {
             )
         )}
       </div>
-      {trailerurl && <Youtube videoid={trailerurl} opts={opts} />}
+      {trailerurl && <YouTube videoId={trailerurl} opts={opts} />}
     </div>
   );
 }
